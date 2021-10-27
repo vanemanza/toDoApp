cac977a16e16329e72df983a3dc5e989b83b2475
VaneManzanelli <vanemanza1@gmail.com>
2021-10-27 15:22:55 -0300

    modifiqué static

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!lb^xhbl!-6l#d=kcc8mj6c&yx_(=@)e#iu!zdf#rf6-hy062w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
